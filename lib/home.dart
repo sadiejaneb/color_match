e7a987cac77331893f54dd529889cea193c31aef
williamchoi7090 <78124946+williamchoi7090@users.noreply.github.com>
2023-11-29 18:41:21 -0500

    Add files via upload

--- lib/home.dart
+++ lib/home.dart
@@ -1,25 +1,43 @@
 import 'package:flutter/material.dart';
 // Import your MuteUnmuteButton widget
 import 'mute_unmute_button.dart'; 
+import 'accomplishments.dart';
+
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Color Match'),
+        title: const Text('Color Match'),
       ),
       body: Stack(
         children: [
           Center(
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(
-                    context, '/gameplay'); // Navigate to GameplayDesign
-              },
-              child: Text('Start'),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                 onPressed: () {
+                    Navigator.pushNamed(
+                        context, '/gameplay'); // Navigate to GameplayDesign
+                 },
+                 child: const Text('Start'),
+                ),
+                ElevatedButton(
+                 onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Accomplishments()),
+                    );
+                 },
+                 child: const Text('View Accomplishments'),
+                ),
+              ],
             ),
           ),
-          Positioned(
+          const Positioned(
             top: 20, 
             right: 20, 
             child: MuteUnmuteButton(), 
